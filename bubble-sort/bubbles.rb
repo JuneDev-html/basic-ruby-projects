f2eefe6c014eb1906f475c0850d97a3ecdf479f9
June <adasilva.html@gmail.com>
2022-12-20 11:05:16 -0500

    Finalize sorting logic

--- bubble-sort/bubbles.rb
+++ bubble-sort/bubbles.rb
@@ -1,6 +1,22 @@
 bubbles = [4,3,78,2,0,2]
 
 def bubble_sort(bubbles)
-  bubbles.sort()
+  bubbles.each_with_index do |bubble, idx|
+    if bubbles[idx + 1] != nil && bubble > bubbles[idx + 1] 
+      empty = bubble
+      bubbles[idx] = bubbles[idx + 1]
+      bubbles[idx + 1] = empty
+      p bubbles
+    end
+  end 
+  bubbles.each_with_index do |bubble, idx|
+    if bubbles[idx + 1] != nil && bubble > bubbles[idx + 1]
+      bubble_sort(bubbles)
+    end
+  end
+  bubbles
 end
+
+bubble_sort(bubbles)
+
 
