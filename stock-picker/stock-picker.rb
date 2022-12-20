19f5f4b7143902f549a945a2ac375f4caf1b1afc
June <adasilva.html@gmail.com>
2022-12-20 09:52:51 -0500

    Finalize stock picker logic

--- stock-picker/stock-picker.rb
+++ stock-picker/stock-picker.rb
@@ -1,8 +1,22 @@
 prices = [17,3,6,9,15,8,6,1,10]
 def stock_picker(prices)
-  prices.filter_map do |price|
-    puts price
+  biggest_difference = -1.0/0.0
+  best_return = []
+  # start pointer 1 holding left position
+  prices.each_with_index do |left_pointer, l_idx|
+    # start pointer 2 moving along right position but only if its after pointer 1
+    prices.each_with_index do |right_pointer, r_idx|\
+      if l_idx < r_idx #only if number is after pointer 1
+        if (right_pointer - left_pointer) > biggest_difference # if the difference between the two is the biggest yet, update info
+          biggest_difference = right_pointer - left_pointer
+          best_return[0] = left_pointer
+          best_return[1] = right_pointer
+        end
+      end
+    end
   end
+
+  best_return
 end
 
-stock_picker(prices)
+p stock_picker(prices)
