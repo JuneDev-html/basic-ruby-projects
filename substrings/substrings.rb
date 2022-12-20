e84c26f0a6ab60ea254910b256f0e83c178e7cd1
June <adasilva.html@gmail.com>
2022-12-19 19:42:39 -0500

    Find match multiple times

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,10 +1,10 @@
-dictionary= ["mediocre","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 def substring(input, dictionary)
   dictionary.reduce(Hash.new(0)) do |results, word| 
-    results[word] += 1 if input.downcase.include?(word) #can only get it to recognize match once here
+    results[word] = input.downcase.scan(word).length if input.downcase.include?(word) 
     results
   end
 end
 
- p substring("Howdy partner, sit down! How's it going?", dictionary)
+dictionary = ["mediocre","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ 
+p substring("Howdy partner, sit down! How's it going?", dictionary)
